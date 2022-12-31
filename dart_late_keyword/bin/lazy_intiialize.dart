4db67f5e927300304052c544ef4f584df5967be1
“ParveenKumar8” <parveenkumar1101@gmail.com>
2022-12-31 10:27:10 +0530

    Late Variable Dependecy Added

--- dart_late_keyword/bin/lazy_intiialize.dart
+++ dart_late_keyword/bin/lazy_intiialize.dart
@@ -1,3 +1,20 @@
+late String name;
 void main(List<String> arguments) {
-  print('Hello world!');
+  try {
+    print(name);
+  } catch (e) {
+    print(e);
+  }
+  name = "Sonu";
+  print(name);
+  print("----------------");
+  print("Before the function");
+  late final newDemo = getName();
+  print("After the function");
+  print(newDemo);
+}
+
+String getName() {
+  print("Function called");
+  return "New Demo";
 }
