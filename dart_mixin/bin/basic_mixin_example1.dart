ba36413f4295f63f968418848ce517115f08e5ec
“ParveenKumar8” <parveenkumar1101@gmail.com>
2023-01-14 10:53:47 +0530

    Simple mixin example 1 finished

--- dart_mixin/bin/basic_mixin_example1.dart
+++ dart_mixin/bin/basic_mixin_example1.dart
@@ -1,3 +1,30 @@
 void main(List<String> arguments) {
-  print('Hello world!');
+  final person = Person();
+  person.jump(speed: 20.0);
+  person.walk(speed: 5.0);
+}
+
+mixin HasSpeed {
+  abstract double speed;
+}
+
+mixin CanJump on HasSpeed {
+  void jump({required double speed}) {
+    print("$runtimeType is jumping at speed of $speed km/h ");
+    this.speed = speed;
+  }
+}
+
+mixin CanWalk on HasSpeed {
+  void walk({required double speed}) {
+    print("$runtimeType is walking at speed of $speed km/h ");
+    this.speed = speed;
+  }
+}
+
+class Person with HasSpeed, CanJump, CanWalk {
+  @override
+  double speed;
+
+  Person() : speed = 0.0;
 }
