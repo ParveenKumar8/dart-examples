c90d9c887dc460e07a31d91dd11e9a7af8597143
“ParveenKumar8” <parveenkumar1101@gmail.com>
2023-01-16 10:28:32 +0530

    String Extensions Example 1

--- dart_extensions/bin/string_ext_example1.dart
+++ dart_extensions/bin/string_ext_example1.dart
@@ -1,3 +1,8 @@
 void main(List<String> arguments) {
-  print('Hello world!');
+  final str = "Hello World !!!";
+  print(str.reversedStr);
+}
+
+extension on String {
+  String get reversedStr => split('').reversed.join().toUpperCase();
 }
