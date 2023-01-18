4df0be31476c5dab48f3fc205aafb8afe4d1e516
“ParveenKumar8” <parveenkumar1101@gmail.com>
2023-01-18 15:41:07 +0530

    Generics - DataType modified Example1

--- dart_generics/bin/datatype_example1.dart
+++ dart_generics/bin/datatype_example1.dart
@@ -1,3 +1,17 @@
 void main(List<String> arguments) {
-  print('Hello world!');
+  final type1 = eitherIntorDouble(TypeToReturn.integer);
+  print(type1);
+  final type2 = eitherIntorDouble(TypeToReturn.double);
+  print(type2);
+}
+
+enum TypeToReturn { integer, double }
+
+T eitherIntorDouble<T extends num>(TypeToReturn typeToReturn) {
+  switch (typeToReturn) {
+    case TypeToReturn.integer:
+      return 5 as T;
+    case TypeToReturn.double:
+      return 5.5 as T;
+  }
 }
